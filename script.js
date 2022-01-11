7e5619e57f78f5d5032f1c79637047a8b7dcd42c
AKP <91975645+AnonymousKnownPerson@users.noreply.github.com>
2022-01-11 01:16:20 +0100

    Comments+Improved AI

--- script.js
+++ script.js
@@ -13,7 +13,10 @@ var config = {
 };
 var game = new Phaser.Game(config);
 function preload() {
+  //wczytanie obrazów/animacji
   this.load.image("background", "assets/amogus map1.png");
+  this.load.image("venticon", "assets/venticon.png");
+  this.load.image("killicon", "assets/killicon.png");
   this.load.image("buton", "assets/block1.png");
   this.load.image("win", "assets/win.png");
   this.load.image("lost", "assets/lost.png");
@@ -38,6 +41,8 @@ let textToDialog = [
 ];
 let scoreText;
 var lost;
+let killicon;
+let venticon;
 let amogus;
 let textDialog;
 let vent;
@@ -51,6 +56,7 @@ let sus = true;
 let talking = false;
 let przegrana = false;
 let susSituation = false;
+let checkIfChancePassed = false;
 let killingDate = Math.round(Date.now() / 1000);
 let talkingDate = Math.round(Date.now() / 1000);
 function create() {
@@ -65,13 +71,15 @@ function create() {
   });
   textDialog = this.add.text(0, 50, "", {
     fontSize: "45px",
-    fill: "#999",
+    fill: "#000000",
+    backgroundColor: "#999999",
   });
   player.setBounce(0);
   player.setCollideWorldBounds(true);
   player.body.gravity.y = 500;
   player.setScale(1.0).refreshBody();
   cursors = this.input.keyboard.createCursorKeys();
+  //przygotowanie animacji
   this.anims.create({
     key: "left",
     frames: this.anims.generateFrameNumbers("player", { start: 0, end: 1 }),
@@ -92,11 +100,7 @@ function create() {
   this.cameras.main.setBounds(0, 0, 6000, 300);
   this.physics.world.setBounds(0, 0, 6000, 300);
   this.cameras.main.startFollow(player);
-  amogus = this.physics.add.group({
-    key: "amogus1",
-    repeat: 10,
-    setXY: { x: 100, y: 300, stepX: 300 },
-  });
+  //przycisk
   buton = this.physics.add.staticGroup();
   buton
     .create(2700, 110, "buton")
@@ -104,12 +108,19 @@ function create() {
     .setOrigin(0)
     .refreshBody()
     .setVisible(false);
+  amogus = this.physics.add.group({
+    key: "amogus1",
+    repeat: 10,
+    setXY: { x: 100, y: 300, stepX: 300 },
+  });
   amogus.children.iterate((child) => {
+    //ustawienia przeciwników
     child.setBounce(1);
     child.body.gravity.y = 500;
     child.setScale(1.0).refreshBody();
     child.setCollideWorldBounds(true);
     child.setVelocityX(Phaser.Math.FloatBetween(-100, 100));
+    //ustawienie animacji
     this.anims.create({
       key: "left1",
       frames: this.anims.generateFrameNumbers("amogus1", { start: 0, end: 1 }),
@@ -128,6 +139,7 @@ function create() {
       repeat: -1,
     });
     child.body.setEnable();
+    //kill
     this.physics.add.overlap(player, child, function () {
       if (
         !isInVents &&
@@ -139,8 +151,17 @@ function create() {
         susSituation = true;
         killingDate = Math.round(Date.now() / 1000);
         scoreText.setText("Enemies : " + howManyToKill);
+        killicon.setVisible(false);
       }
     });
+    //kill-icon
+    this.physics.add.overlap(player, child, function () {
+      if (!isInVents && Math.round(Date.now() / 1000) - killingDate > 2) {
+        killicon.setVisible(true);
+        checkIfChancePassed = true;
+      }
+    });
+    //button-przegrana
     this.physics.add.overlap(buton, child, function () {
       if (
         przegrana ||
@@ -151,7 +172,25 @@ function create() {
         player.setVisible(false);
       }
     });
+    //dialog-hołownia
+    this.physics.add.overlap(player, child, function () {
+      if (
+        !isInVents &&
+        cursors.shift.isDown &&
+        Math.round(Date.now() / 1000) - talkingDate > 3 &&
+        child.body.velocity.x < 399 &&
+        child.body.velocity.x > -399
+      ) {
+        talking = true;
+        talkingDate = Math.round(Date.now() / 1000);
+        textDialog.x = child.body.position.x;
+        textDialog.setText(textToDialog[Math.floor(Math.random() * 7)]);
+        textDialog.setVisible(true);
+        child.setVelocityX(0);
+      }
+    });
   });
+  //rozłożenie ventów
   vents = this.physics.add.staticGroup();
   vents.create(30, 200, "vent");
   vents.create(1520, 200, "vent");
@@ -161,13 +200,35 @@ function create() {
     child.setScale(1).setOrigin(0).refreshBody();
     child.setVisible(false);
   });
+  //wyczuwanie gracza koło ventów
   this.physics.add.overlap(vents, player, function () {
     inZone = true;
   });
+  //przygotowanie ikon
+  venticon = this.add.image(850, 250, "venticon");
+  venticon.setScale(0.1, 0.1).setScrollFactor(0);
+  venticon.setVisible(false);
+  killicon = this.add.image(750, 250, "killicon");
+  killicon.setScale(0.1, 0.1).setScrollFactor(0);
+  killicon.setVisible(false);
 }
+let ventIsVisible = false;
+let killIsVisible = false;
 function update() {
+  if (!checkIfChancePassed) killicon.setVisible(false);
+  //napis pod player
   scoreText.setText("Enemies : " + howManyToKill);
   scoreText.x = player.body.position.x;
+  //ikonka venta
+  if (inZone && !ventIsVisible) {
+    venticon.setVisible(true);
+    ventIsVisible = true;
+  } else if (inZone && ventIsVisible) {
+  } else {
+    venticon.setVisible(false);
+    ventIsVisible = false;
+  }
+  //venty
   if (inZone && cursors.down.isDown) {
     isInVents = true;
     susSituation = true;
@@ -175,6 +236,7 @@ function update() {
     isInVents = false;
     susSituation = true;
   }
+  //poruszanie się player(vent albo nie)
   if (isInVents) {
     player.setVisible(false);
     if (cursors.left.isDown) {
@@ -194,7 +256,7 @@ function update() {
       player.setVelocityX(0);
     }
   }
-
+  //animacje player
   if (cursors.left.isDown) {
     player.anims.play("left", true);
   } else if (cursors.right.isDown) {
@@ -202,34 +264,39 @@ function update() {
   } else {
     player.anims.play("front");
   }
+  //wygrana
   if (howManyToKill == 0 || drip == true) {
     let winned = this.add.tileSprite(0, 0, 900, 300, "win");
     winned.setOrigin(0);
     player.x = 0;
     player.y = 0;
     drip = true;
+    venticon.setVisible(false);
+    killicon.setVisible(false);
   }
+  //pętla po przeciwnikach
   amogus.children.iterate((child) => {
+    //wartość by sprawdzić jak blisko jest przeciwnik
     let x = Math.abs(player.x - child.x);
+    //gdy przeciwnik przestanie rozmawiać nadana jest mu prędkość
     if (child.body.velocity.x == 0 && !talking)
       child.setVelocityX(Phaser.Math.FloatBetween(-100, 100));
+    //zakończenie rozmowy
     if (talking && Math.round(Date.now() / 1000) - talkingDate > 3) {
       talking = false;
       textDialog.setVisible(false);
-    } else {
     }
+    //czy przeciwnik zauważył, że jesteś SUS
     if (susSituation && x < 450) {
-      if (player.x - child.x > 0 && (child.body.velocity.x > 0 || talking)) {
+      if (player.x - child.x > 0 && child.body.velocity.x >= 0) {
         child.setVelocityX(-400);
         textDialog.setVisible(false);
-      } else if (
-        player.x - child.x < 0 &&
-        (child.body.velocity.x < 0 || talking)
-      ) {
+      } else if (player.x - child.x < 0 && child.body.velocity.x <= 0) {
         child.setVelocityX(400);
         textDialog.setVisible(false);
       }
     }
+    //przeciwnicy klatki animacji
     if (child.body.velocity.x < 0) {
       child.anims.play("left1", true);
     } else if (child.body.velocity.x > 0) {
@@ -237,23 +304,8 @@ function update() {
     } else {
       child.anims.play("front1");
     }
-    this.physics.add.overlap(player, child, function () {
-      if (
-        !isInVents &&
-        cursors.shift.isDown &&
-        Math.round(Date.now() / 1000) - talkingDate > 3 &&
-        child.body.velocity.x < 399 &&
-        child.body.velocity.x > -399
-      ) {
-        talking = true;
-        talkingDate = Math.round(Date.now() / 1000);
-        textDialog.x = child.body.position.x;
-        textDialog.setText(textToDialog[Math.floor(Math.random() * 7)]);
-        textDialog.setVisible(true);
-        child.setVelocityX(0);
-      }
-    });
   });
+  //przegrana
   if (przegrana == true) {
     lost.setOrigin(0);
     lost.setVisible(true);
@@ -262,8 +314,11 @@ function update() {
     player.setVisible(false);
     amogus.setVisible(false);
     scoreText.setVisible(false);
+    venticon.setVisible(false);
+    killicon.setVisible(false);
   }
   inZone = false;
   rangeToKill = false;
   susSituation = false;
+  checkIfChancePassed = false;
 }
